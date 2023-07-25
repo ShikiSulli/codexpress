c3f043f6c1eb45b7015558f231751d5cceeeddf9
Jensone <hello@jensone.com>
2023-07-25 08:11:33 +0200

    🐛 fix(PageController.php): update route annotation to use root route '/' instead of '/page' for the homepage ✨ feat(PageController.php): add pagination functionality to the index action to display snippets in a paginated manner 🐛 fix(RegistrationFormType.php): update label for the agreeTerms checkbox to be in French 🐛 fix(RegistrationFormType.php): update label for the plainPassword field to be in French 🐛 fix(RegistrationFormType.php): update validation constraints for the username field to be in French 🐛 fix(RegistrationFormType.php): update validation constraints for the email field to be in French 🐛 fix(RegistrationFormType.php): update validation constraints for the plainPassword field to be in French 🐛 fix(base.html.twig): update page title to include the name of the application ✨ feat(base.html.twig): add link to external CSS library for styling ✨

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,23 +3,38 @@
 namespace App\Controller;
 
 use App\Repository\SnippetRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
+    // Route de la page d'accueil
+    #[Route('/', name: 'app_page')]
     public function index(
-        SnippetRepository $snippets
+        SnippetRepository $snippets, // Chargement du repository Snippet
+        PaginatorInterface $paginator, // Chargement de PaginatorInterface
+        Request $request // Chargement de Request
     ): Response
     {
+        // On créer une requête pour récupérer les snippets
+        $query = $snippets->findBy(
+            ['isPublished' => true], // Pour sélectionner
+            ['createdAt' => 'DESC'], // Pour trier
+            100 // Pour limiter l'affichage
+        );
+
+        // On utilise le paginator pour paginer les snippets
+        $pagination = $paginator->paginate(
+            $query, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, 1 par défaut
+            10 // Nombre de résultats par page
+        );
+        
         return $this->render('page/index.html.twig', [
-            'snippets' => $snippets->findBy(
-                ['isPublished' => true], // Pour sélectionner
-                ['createdAt' => 'DESC'], // Pour trier
-                10 // Pour limiter l'affichage
-            )
+            'snippets' => $pagination
         ]);
     }
 }
