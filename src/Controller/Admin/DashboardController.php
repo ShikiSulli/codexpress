48a2f82c5cbbbba45ce6b89859e3c3c280d7cb92
Jensone <hello@jensone.com>
2023-07-28 14:11:17 +0200

    🔧 chore(DashboardController.php): refactor DashboardController to improve code structure and readability ✨ feat(DashboardController.php): add support for creating and rendering a line chart using Symfony UX Chartjs 🔧 chore(DashboardController.php): update configureDashboard method to set a custom title with CodeXpress logo 🔧 chore(DashboardController.php): update configureMenuItems method to add a link to logout functionality

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,30 +2,67 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Snippet;
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Snippet;
+use Symfony\UX\Chartjs\Model\Chart;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+    private ChartBuilderInterface $chartBuilder;
+
+    public function __construct(ChartBuilderInterface $chartBuilder)
+    {
+        $this->chartBuilder = $chartBuilder;
+    }
+
+    private function createChart(): Chart
+    {
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+        return $chart;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        
-        return $this->render('admin/dashboard.html.twig');
+
+        return $this->render('admin/dashboard.html.twig', [
+            'chart' => $this->createChart(),
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('CodeXpress');
+            ->setTitle('<img src="/images/logo-codexpress.svg" width="300" alt="CodeXpress Partage de code">');
     }
 
     public function configureMenuItems(): iterable
@@ -36,6 +73,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
         yield MenuItem::section('Autres');
         yield MenuItem::linkToRoute('Retour à l\'accueil', 'fas fa-arrow-left', 'app_page');
+        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-arrow-right-from-bracket');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
