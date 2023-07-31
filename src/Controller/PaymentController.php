fe9d8c967193c9b75f573fe407ec4db2d2a8ee7b
Jensone <hello@jensone.com>
2023-07-31 13:56:25 +0200

    🐛 fix(PaymentController.php): fix typo in variable name $request in index() method ✨ feat(PaymentController.php): refactor payment process to use Stripe Checkout Session for better user experience ✨ feat(PaymentController.php): add success and cancel routes for handling payment success and cancellation respectively ✨ feat(PaymentController.php): implement logic to update user role and isPremium status upon successful payment ✨ feat(PaymentController.php): add flash messages and redirect users to appropriate pages after payment success or cancellation

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,33 +2,71 @@
 
 namespace App\Controller;
 
-use Stripe\Charge;
+use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Stripe;
+use Stripe\Checkout\Session;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class PaymentController extends AbstractController
 {
     #[Route('/payment', name: 'app_payment')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $token = $this->$request->request->get('stripeToken');
+        // Initialisation de la clé API de Stripe        
         Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
-        Charge::create ([
-                "amount" => 10 * 100,
-                "currency" => "eur",
-                "source" => $token,
-                "description" => "Abonnement CodeXpress"
+
+        // On créer une session de paiement
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => 1000,
+                    'product_data' => [
+                        'name' => 'Abonnement Premium',
+                        'images' => ["https://raw.githubusercontent.com/Jensone/codexpress/master/public/images/logo-codexpress.png"],
+                    ],
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('app_payment_success', [], 0),
+            'cancel_url' => $this->generateUrl('app_payment_cancel', [], 0),
         ]);
-        $this->addFlash(
-            'success',
-            'Paiement validé'
-        );
-        return $this->redirectToRoute('app_page', [], Response::HTTP_SEE_OTHER);
-        return $this->render('payment/index.html.twig', [
-            'controller_name' => 'PaymentController',
-        ]);
+
+        return $this->redirect($session->url, 303);
+    }
+
+    #[Route('/payment/success', name: 'app_payment_success')]
+    public function success(EntityManagerInterface $em): Response
+    {
+        // On récupère l'utilisateur connecté
+        $user = $this->getUser();
+
+        // On lui donne le rôle ROLE_PREMIUM
+        $user->setRoles(["ROLE_PREMIUM"]);
+        $user->setIsPremium(true);
+
+        // On enregistre l'utilisateur en base de données
+        $em->persist($user);
+        $em->flush();
+
+        // On envoye un message flash
+        $this->addFlash('message', 'Le paiement a bien été effectué. Vous êtes désormais Premium !');
+
+        // On redirige l'utilisateur vers la page d'accueil
+        return $this->redirectToRoute('app_page');
+    }
+
+    #[Route('/payment/cancel', name: 'app_payment_cancel')]
+    public function cancel(): Response
+    {
+        // On envoye un message flash
+        $this->addFlash('message', 'Le paiement a n\'a pas abouti. Réessayez.');
+
+        // On redirige l'utilisateur vers la page d'accueil
+        return $this->redirectToRoute('app_page');
     }
 }
